175cbf4d95dc050d1e63352881455c2401957dfb
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-20 00:09:59 -0300

    agregue imagen a carpeta imgError404 y cree estructura

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,13 @@
+import error from "../../assets/error404img.png";
+import { Link } from "react-router";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Error404 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Error404;
