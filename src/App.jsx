cf997495972d43c16477fe4af834964fc7214bfc
Nacho <barrojojoaquin2@gmail.com>
2025-07-21 23:04:25 -0300

    agrega el formulario para añadir juegos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/shared/Header";
 import Error404 from "./components/pages/Error404";
 import ProtectorAdmin from "./components/routes/ProtectorAdmin"
 import Inicio from "./components/pages/Inicio";
+import FormularioProducto from "./components/pages/Productos/FormularioProducto";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             {/* falta ruta de inicio, protector de rutas y ruta a Administrador */}
             <Route path="/" element={<Inicio></Inicio>}></Route>
+            <Route path="/crear" element={<FormularioProducto></FormularioProducto>}></Route>
             <Route path="*" element={<Error404></Error404>}></Route>
           </Routes>
         </main>
