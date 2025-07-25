4f5e3bf5eb30c7f83ba0ba54b0e1bc12d754c5bc
Lucho <diaz.luis.10911@gmail.com>
2025-07-25 11:14:01 -0300

    Se agrego la seccion de Tecnologia que dominamos del componente SobreNosotros

--- src/components/pages/SobreNosotros.jsx
+++ src/components/pages/SobreNosotros.jsx
@@ -4,6 +4,7 @@ const SobreNosotros = () => {
       {/* Galeria del equipo */}
       <section className="container my-5">
         <h2 className="bebasneue display-6 text-center">Conoce a nuestro equipo</h2>
+        {/* Inicio galeria del equipo */}
         <div class="row justify-content-center row-gap-3">
           {/* Pimer miembro del equipo */}
           <article className="col-12 col-md-6 col-lg-3 equipoCard">
@@ -42,7 +43,7 @@ const SobreNosotros = () => {
             </div>
           </article>
           {/* Tercer miembro del equipo */}
-                    <article className="col-12 col-md-6 col-lg-3 equipoCard">
+          <article className="col-12 col-md-6 col-lg-3 equipoCard">
             <div class="card shadow h-100">
               {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
               <div className="card-main bg-white text-dark h-100 my-2">
@@ -60,7 +61,7 @@ const SobreNosotros = () => {
             </div>
           </article>
           {/* Cuarto miembro del equipo */}
-                    <article className="col-12 col-md-6 col-lg-3 equipoCard">
+          <article className="col-12 col-md-6 col-lg-3 equipoCard">
             <div class="card shadow h-100">
               {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
               <div className="card-main bg-white text-dark h-100 my-2">
@@ -78,7 +79,7 @@ const SobreNosotros = () => {
             </div>
           </article>
           {/* Quinto miembro del equipo */}
-                    <article className="col-12 col-md-6 col-lg-3 equipoCard">
+          <article className="col-12 col-md-6 col-lg-3 equipoCard">
             <div class="card shadow h-100">
               {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
               <div className="card-main bg-white text-dark h-100 my-2">
@@ -96,8 +97,46 @@ const SobreNosotros = () => {
             </div>
           </article>
         </div>
+        {/* Fin galeria del equipo */}
+        {/* Inicio Tecnologia que dominamos */}
+        <div className="container my-5">
+          <div className="card shadow rounded p-4">
+            <h2 className="text-center mb-4">Tecnologías que dominamos</h2>
+            <div className="row text-center justify-content-center align-items-center">
+              {/* HTML */}
+              <div className="col-6 col-md-2 mb-4">
+                <img src="" alt="" className="img-fluid mb-2" />
+                <p>HTML5</p>
+              </div>
+
+              {/*CSS3 */}
+              <div className="col-6 col-md-2 mb-4">
+                <img src="" alt="" className="img-fluid mb-2" />
+                <p>CSS3</p>
+              </div>
+
+              {/* JavaScript */}
+              <div className="col-6 col-md-2 mb-4">
+                <img src="" alt="" className="img-fluid mb-2" />
+                <p>JavaScript</p>
+              </div>
+
+              {/* React */}
+              <div className="col-6 col-md-2 mb-4">
+                <img src="" alt="" className="img-fluid mb-2" />
+                <p>React</p>
+              </div>
+
+              {/* Bootstrap */}
+              <div className="col-6 col-md-2 mb-4">
+                <img src="" alt="" className="img-fluid mb-2" />
+                <p>Bootstrap</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Fin Tecnologia que dominamos */}
       </section>
-      {/* Galeria del equipo */}
     </>
   );
 };
