0e1a268f3b238a7928e24a3337ec15be3bcaf166
Lucho <diaz.luis.10911@gmail.com>
2025-07-25 15:17:29 -0300

    Se agregaron las imagenes de los avatars para cada uno de los miembros del equipo, tambien se arreglaron los logos en la parte de Tecnologias que dominamos en el componente SobreNosotros

--- src/components/pages/SobreNosotros.jsx
+++ src/components/pages/SobreNosotros.jsx
@@ -1,9 +1,13 @@
-import htmlologo from '../img/imgSobreNosotros/html.png'
-import csslogo from '../img/imgSobreNosotros/css.png'
-import javascriptlogo from '../img/imgSobreNosotros/javascript.png'
-import reactlogo from '../img/imgSobreNosotros/react.png'
-import boostraplogo from '../img/imgSobreNosotros/bootstrap.png'
-
+import htmlologo from "../img/imgSobreNosotros/html.png";
+import csslogo from "../img/imgSobreNosotros/css.png";
+import javascriptlogo from "../img/imgSobreNosotros/javascript.png";
+import reactlogo from "../img/imgSobreNosotros/react.png";
+import bootstraplogo from "../img/imgSobreNosotros/bootstrap.png";
+import avatar1 from "../img/imgSobreNosotros/avatar1.png";
+import avatar2 from "../img/imgSobreNosotros/avatar2.png";
+import avatar3 from "../img/imgSobreNosotros/avatar3.png";
+import avatar4 from "../img/imgSobreNosotros/avatar4.png";
+import avatar5 from "../img/imgSobreNosotros/avatar5.png";
 
 const SobreNosotros = () => {
   return (
@@ -12,10 +16,12 @@ const SobreNosotros = () => {
         {/* Inicio presentacion */}
         <div className="container my-5">
           <h2 className="d-flex justify-content-center mb-4">GamerZone</h2>
-          <blockquote class="bebasneue mb-0 fst-italic text-center text-secondary">
-            "Somos el equipo detrás de GamerZone, desarrolladores apasionados por los videojuegos y la tecnología. Nos
-            especializamos en crear el mejor catálogo de juegos con categorías únicas, ofreciendo experiencias gaming
-            inolvidables."
+          <blockquote class=" mb-0 fst-italic text-center text-secondary">
+            "Somos el equipo detrás de GamerZone, desarrolladores apasionados por los videojuegos y la tecnología.
+          </blockquote>
+          <blockquote className=" mb-0 fst-italic text-center text-secondary">
+            Nos especializamos en crear el mejor catálogo de juegos con categorías únicas, ofreciendo experiencias
+            gaming inolvidables."
           </blockquote>
         </div>
         {/* Fin presentacion */}
@@ -24,11 +30,11 @@ const SobreNosotros = () => {
           <h2 className="bebasneue display-6 text-center">Conoce a nuestro equipo</h2>
           <div class="row justify-content-center row-gap-3">
             {/* Pimer miembro del equipo */}
-            <article className="col-12 col-md-6 col-lg-3 equipoCard">
-              <div class="card shadow h-100">
-                {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
-                <div className="card-main bg-white text-dark h-100 my-2">
-                  <h5 className="card-title bebasneue text-center fs-4">Hernan Ortiz</h5>
+            <article className="col-6 col-md-4 col-lg-2 equipoCard">
+              <div class="card border-0 shadow h-100">
+                <img src={avatar1} alt="Avatar del primer miembro del equipo" />
+                <div className="card-main bg-white text-dark h-100 py-2">
+                  <h5 className="card-title bebasneue text-center fs-4 mt-3">Hernan Ortiz</h5>
                   <p className="text-center lead card-text">Especialista en React</p>
                   <p className="roboto mx-3 text-center">
                     4 años de experiencia desarrollando interfaces de usuario responsivas y accesibles.
@@ -42,11 +48,11 @@ const SobreNosotros = () => {
               </div>
             </article>
             {/* Segundo miembro del equipo */}
-            <article className="col-12 col-md-6 col-lg-3 equipoCard">
-              <div class="card shadow h-100">
-                {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
-                <div className="card-main bg-white text-dark h-100 my-2">
-                  <h5 className="card-title bebasneue text-center fs-4">Marcos Joel Tebis</h5>
+            <article className="col-6 col-md-4 col-lg-2 equipoCard">
+              <div class="card border-0 shadow h-100">
+                <img src={avatar2} alt="Avatar del segundo miembro del equipo" />
+                <div className="card-main bg-white text-dark h-100 py-2">
+                  <h5 className="card-title bebasneue text-center fs-4 mt-3">Marcos Joel Tebis</h5>
                   <p className="text-center lead card-text">Experto en CSS</p>
                   <p className="roboto mx-3 text-center">
                     Diseñador de sistemas de diseño y maquetación con enfoque mobile-first.
@@ -60,11 +66,11 @@ const SobreNosotros = () => {
               </div>
             </article>
             {/* Tercer miembro del equipo */}
-            <article className="col-12 col-md-6 col-lg-3 equipoCard">
-              <div class="card shadow h-100">
-                {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
-                <div className="card-main bg-white text-dark h-100 my-2">
-                  <h5 className="card-title bebasneue text-center fs-4">Jose Benjamin Quiros</h5>
+            <article className="col-6 col-md-4 col-lg-2 equipoCard">
+              <div class="card border-0 shadow h-100">
+                <img src={avatar3} alt="Avatar del tercer miembro del equipo" />
+                <div className="card-main bg-white text-dark h-100 py-2">
+                  <h5 className="card-title bebasneue text-center fs-4 mt-3">Jose Benjamin Quiros</h5>
                   <p className="text-center lead card-text">Backend y BOM</p>
                   <p className="roboto mx-3 text-center">
                     Especialista en operaciones CRUD, API's y manipulación de DOM/BOM.
@@ -78,11 +84,11 @@ const SobreNosotros = () => {
               </div>
             </article>
             {/* Cuarto miembro del equipo */}
-            <article className="col-12 col-md-6 col-lg-3 equipoCard">
-              <div class="card shadow h-100">
-                {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
-                <div className="card-main bg-white text-dark h-100 my-2">
-                  <h5 className="card-title bebasneue text-center fs-4">Ignacio Joaquin Barrojo</h5>
+            <article className="col-6 col-md-4 col-lg-2 equipoCard">
+              <div class="card border-0 shadow h-100">
+                <img src={avatar4} alt="Avatar del cuarto miembro del equipo" />
+                <div className="card-main bg-white text-dark h-100 py-2">
+                  <h5 className="card-title bebasneue text-center fs-4 mt-3">Ignacio Joaquin Barrojo</h5>
                   <p className="text-center lead card-text">Game Curator</p>
                   <p className="roboto mx-3 text-center">
                     Especialista en seleccionar y evaluar los mejores juegos para nuestro catálogo.
@@ -96,11 +102,11 @@ const SobreNosotros = () => {
               </div>
             </article>
             {/* Quinto miembro del equipo */}
-            <article className="col-12 col-md-6 col-lg-3 equipoCard">
-              <div class="card shadow h-100">
-                {/* <img src="#Link" className="card-img-top equipoFoto col-6" alt=""> */}
-                <div className="card-main bg-white text-dark h-100 my-2">
-                  <h5 className="card-title bebasneue text-center fs-4">Luis Martin Diaz</h5>
+            <article className="col-6 col-md-4 col-lg-2 equipoCard">
+              <div class="card border-0 shadow h-100">
+                <img src={avatar5} alt="Avatar del quinto miembro del equipo" />
+                <div className="card-main bg-white text-dark h-100 py-2">
+                  <h5 className="card-title bebasneue text-center fs-4 mt-3">Luis Martin Diaz</h5>
                   <p className="text-center lead card-text">Arquitecto Frontend</p>
                   <p className="roboto mx-3 text-center">
                     Lider técnico con experiencia en proyectos complejos de JavaScript.
@@ -118,37 +124,37 @@ const SobreNosotros = () => {
         {/* Fin galeria del equipo */}
         {/* Inicio Tecnologia que dominamos */}
         <div className="container my-5">
+          <h2 className="text-center mb-4">Tecnologías que dominamos</h2>
           <div className="card shadow rounded p-4">
-            <h2 className="text-center mb-4">Tecnologías que dominamos</h2>
             <div className="row text-center justify-content-center align-items-center">
               {/* HTML */}
-              <div className="col-6 col-md-2 mb-4">
-                <img src={htmlologo} alt="" className="img-fluid mb-2" />
+              <div className="col-6 col-md-2 mb-0">
+                <img src={htmlologo} alt="Logo de HTML" className="img-fluid mb-2" />
                 <i></i>
                 <p>HTML5</p>
               </div>
 
               {/*CSS3 */}
-              <div className="col-6 col-md-2 mb-4">
-                <img src={csslogo} alt="" className="img-fluid mb-2" />
+              <div className="col-6 col-md-2 mb-0">
+                <img src={csslogo} alt="Logo de CSS" className="img-fluid mb-2" />
                 <p>CSS3</p>
               </div>
 
               {/* JavaScript */}
-              <div className="col-6 col-md-2 mb-4">
-                <img src={javascriptlogo} alt="" className="img-fluid mb-2" />
+              <div className="col-6 col-md-2 mb-0">
+                <img src={javascriptlogo} alt="Logo de JavaScript" className="img-fluid mb-2" />
                 <p>JavaScript</p>
               </div>
 
               {/* React */}
-              <div className="col-6 col-md-2 mb-4 w-16 h-16 mb-3 animate-float">
-                <img src={reactlogo} alt="" className="img-fluid mb-2" />
-                <span className="font-medium">React</span>
+              <div className="col-6 col-md-2 mb-0">
+                <img src={reactlogo} alt="Logo de React" className="img-fluid mb-2" />
+                <p className="font-medium">React</p>
               </div>
 
               {/* Bootstrap */}
-              <div className="col-6 col-md-2 mb-4">
-                <img src={boostraplogo} alt="" className="img-fluid mb-2" />
+              <div className="col-6 col-md-2 mb-0">
+                <img src={bootstraplogo} alt="Logo de Bootstrap" className="img-fluid mb-2" />
                 <p>Bootstrap</p>
               </div>
             </div>
