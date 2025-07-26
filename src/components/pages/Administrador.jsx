41b975f8fca5cfc14350e389644c7e6dcc85809f
Nacho <barrojojoaquin2@gmail.com>
2025-07-26 13:49:43 -0300

    fixeo de varios problemas con el maquetado

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,23 +1,22 @@
 import { Table, Container, Button } from "react-bootstrap";
 import ItemJuegos from "./Juegos/ItemJuegos";
 import { Link } from "react-router";
-import {juegosPrueba} from "../../data/juegosPrueba"
-const Administrador = ({setJuegos, juegos}) => {
-    const cargarJuegos=()=>{
-        setJuegos(juegosPrueba)
-    }
+import { juegosPrueba } from "../../data/juegosPrueba";
+const Administrador = ({ setJuegos, juegos, borrarJuego }) => {
+  const cargarJuegos = () => {
+    setJuegos(juegosPrueba);
+  };
   return (
     <>
       <Container className="my-2">
         <div className="d-flex justify-content-between align-items-center mt-5">
           <h1 className="display-4 ">Juegos disponibles</h1>
+          
           <div>
             <Link className="btn btn-primary" to={"/administrador/crear"}>
               <i className="bi bi-file-earmark-plus"></i>
             </Link>
-            <Button className="ms-2 btn btn-info text-light"
-            onClick={cargarJuegos}
-            >
+            <Button className="ms-2 btn btn-info text-light" onClick={cargarJuegos}>
               <i className="bg bi bi-database-fill-add"></i>
             </Button>
           </div>
@@ -36,12 +35,9 @@ const Administrador = ({setJuegos, juegos}) => {
             </tr>
           </thead>
           <tbody>
-            {juegos.map((juego, indice)=> (
-                <ItemJuegos key={juego.id}
-                fila={indice+1}
-                juego={juego}
-                ></ItemJuegos>
+            {juegos.map((juego, indice) => (
 
+              <ItemJuegos key={juego.id} fila={indice + 1} juego={juego} borrarJuego={borrarJuego}></ItemJuegos>
             ))}
           </tbody>
         </Table>
